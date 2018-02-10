32692c248a5aea6c00f33848aa0b27d9deef41a0
Szymon Pucher <spucher@interia.eu>
2018-02-10 14:56:09 +0100

    Proper display of next sunday state.

--- index.php
+++ index.php
@@ -7,9 +7,74 @@
 						<?php include("navigation.php"); ?>
 						<!-- Powiadomienia i sklepy -->
 						<div id="banner">
-							<h2>W najbliższą niedzielę sklepy będą
-							<br /><br />
-							<strong>OTWARTE</strong></h2>
+
+							<?php
+							$handle = fopen("lista_niedziel.txt", "r");
+							date_default_timezone_set('UTC');
+							$day =  date("d");
+							$day_of_week = date("D");
+							$month = date("m");
+							$date = date("d.m");
+
+							$res = 0;
+							if ($handle) {
+							    while (($line = fgets($handle)) !== false) {
+										list($file_day, $file_month) = explode(".", $line);
+										if((int)$file_day == (int)$day && (int)$file_month == (int)$month){
+											$res = 3;
+											break;
+										}
+										for($i = 1; $i < 8; $i++){
+											$date2 = Date("d.m", mktime(0,0,0,(int)$month,(int)$day+$i,2018));
+											list($next_day, $next_month) = explode(".", $date2);
+											if((int)$file_day == (int)$next_day && (int)$file_month == (int)$next_month){
+												$res = 1;
+												break;
+											}
+											else{
+												$res = 2;
+											}
+										}
+										if($res == 1) break;
+
+							    }
+									if($res != 3 && !strcmp("Sun", $day_of_week)){
+										$res = 4;
+									}
+							    fclose($handle);
+							} else {
+							    // error opening the file.
+							}
+
+							switch($res){
+								case 1:
+								echo "<h2>W najbliższą niedzielę sklepy będą
+								<br /><br />
+								<strong>ZAMKNIĘTE</strong></h2>";
+								break;
+								case 2:
+								echo "<h2>W najbliższą niedzielę sklepy będą
+								<br /><br />
+								<strong>OTWARTE</strong></h2>";
+								break;
+								case 3:
+								echo "<h2>Dziś sklepy są
+								<br /><br />
+								<strong>ZAMKNIĘTE</strong></h2>";
+								break;
+								case 4:
+								echo "<h2>Dziś sklepy są
+								<br /><br />
+								<strong>OTWARTE</strong></h2>";
+								break;
+								default:
+								echo "<h2>Brak danych otwarcia sklepów.</h2>
+								<br /><br />";
+
+							}
+							//echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".$date;
+							?>
+
 							<p>Chcesz wiedzieć w którą niedzielę sklepy będą zamknięte?</p>
 							<a href="powiadomienie.php" class="button big">Ustaw<br />powiadomienie</a>
 							<br /><br /><br />
