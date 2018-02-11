2990e33e1a779e9e9308bcaf1bf907a7eaab316b
Adam <adam.pucher@ekinnolab.eu>
2018-02-11 12:44:20 +0100

    added description, keywords and google analytics link

--- header.php
+++ header.php
@@ -2,8 +2,24 @@
 
 <html>
 	<head>
-		<title>Handel w Niedzielę</title>
+		<title>Handlowa Niedziela</title>
+
+		<meta name="description" content="Wiesz czy zrobisz zakupy w Niedzielę">
+		<meta name="keywords" content="handlowa niedziela, zakupy w niedzielę, kupowanie w niedzielę, na zakupy w niedzielę, kiedy wolna niedziela">
+
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="assets/css/main.css" />
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113954044-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-113954044-1');
+		</script>
+
+
 	</head>
