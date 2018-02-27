239b5a8c42be681ae81c28d6b82e1fbf9d30d2d4
Adam <adam.pucher@ekinnolab.eu>
2018-02-27 21:25:03 +0100

    Poprawki SEO

--- header.php
+++ header.php
@@ -4,10 +4,9 @@
 	<head>
 		<title>Handlowa Niedziela</title>
 
-		<meta name="description" content="Wiesz czy zrobisz zakupy w Niedzielę">
+		<meta name="description" content="Handlowa Niedziela i juz wiesz czy zrobisz zakupy">
 		<meta name="keywords" content="handlowa niedziela, zakupy w niedzielę, kupowanie w niedzielę, na zakupy w niedzielę, kiedy wolna niedziela">
-
-		<meta charset="utf-8" />
+  	<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="assets/css/main.css" />
 
